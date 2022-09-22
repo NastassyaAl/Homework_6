11296a470c3bc6dac44d7bec462339e1c6d52343
unknown <nastena-alhovik@yandex.ru>
2022-09-22 22:59:15 +0300

    add Task 43 HomeWork 6

---
+++ Task_43/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.Write("b1 = ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("k1 = ");
+double k1 = int.Parse(Console.ReadLine());
+Console.Write("b2 = ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("k2 = ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+void IntersectionPoint ()
+{
+double x = Math.Round((b2-b1)/(k1-k2),1);
+double y = Math.Round(k1 * x + b1,1);
+Console.WriteLine($"Координаты точки пересечения прямых ({x};{y})");
+}
+IntersectionPoint();
